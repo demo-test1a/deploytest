da737aa1255e1218b1cd9fd764cc24a867c8aab1
Bhavani <moksi.balaji@gmail.com>
2019-12-19 23:06:25 +0530

    1stcommit of deploytest

---
+++ src/main/java/com/example/Testingdeploy/TestingDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.Testingdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingDeployApplication.class, args);
+	}
+
+}
